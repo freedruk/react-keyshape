6b023e1dd92f50ec317a91da0d2801db14203ea4
freedruk <fredrik.ward@gmail.com>
2018-02-06 13:02:47 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Keyshape from './components/Keyshape';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Keyshape/>
+      </div>
+    );
+  }
+}
+
+export default App;
