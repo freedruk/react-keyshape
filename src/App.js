7478ed9d7dbbadf7a496645deac4cd2b4ca32442
freedruk <fredrik.ward@gmail.com>
2018-02-06 13:20:46 +0100

    fixes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Keyshape from './components/Keyshape';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="App">
         <Keyshape/>
-        <div><strong>Icon made by my good friend <a href="https://beta.designforventures.co/Free-Animated-SVG-Icons/">Juan Maguid</a></strong></div>
+        <div><strong>The icon made by my good friend <a href="https://beta.designforventures.co/Free-Animated-SVG-Icons/">Juan Maguid</a></strong></div>
       </div>
     );
   }
